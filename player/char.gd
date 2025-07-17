75d2e91f2bba5a8a49a7376b0bcb47d69a56f979
Bach Le <bachle098@gmail.com>
2025-07-17 10:20:44 -0700

    swing system

--- player/char.gd
+++ player/char.gd
@@ -7,7 +7,10 @@ const TRAVEL_TIME := 0.3
 @onready var left_ray := $CharacterBody3D/leftRay
 @onready var right_ray := $CharacterBody3D/rightRay
 @onready var animation := $animation
-
+var dirVec := Vector2.ZERO
+var dragging := false
+var swing_ready := true
+const SWING_THRESHOLD := 40 
 var tween : Tween
 
 func snap_to_grid(pos: Vector3, grid_size: float = 2.0) -> Vector3:
@@ -20,9 +23,45 @@ func snap_to_grid(pos: Vector3, grid_size: float = 2.0) -> Vector3:
 func _ready():
 	transform.origin = snap_to_grid(transform.origin)
 
+func _input(event):
+	if event is InputEventMouseButton and Input.is_action_pressed("RMB"):
+		if event.pressed:
+			dragging = true
+			dirVec = Vector2.ZERO
+			swing_ready = true
+		else:
+			dragging = false
+			swing_ready = true
+			dirVec = Vector2.ZERO
+
+	elif event is InputEventMouseMotion and dragging:
+		dirVec += event.relative
+		if dirVec.length() >= SWING_THRESHOLD and swing_ready:
+			process_swing()
+
+func process_swing():
+	if animation.is_playing() and (animation.current_animation == "swing left" or animation.current_animation == "swing right" or animation.current_animation == "swing down" or animation.current_animation == "swing up"):
+		return
+	swing_ready = false  # Prevent re-trigger until next swing
+	var direction = dirVec.normalized()
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			animation.play("swing right")
+		else:
+			animation.play("swing left")
+	else:
+		if direction.y > 0:
+			animation.play("swing down")
+		else:
+			animation.play("swing up")
+	dirVec = Vector2.ZERO
+
+
 func _physics_process(_delta):
 	if tween is Tween:
 		if tween.is_running():
+			return
+		if animation.is_playing() and (animation.current_animation == "swing left" or animation.current_animation == "swing right" or animation.current_animation == "swing down" or animation.current_animation == "swing up"):
 			return
 	#if right_ray.is_colliding():
 	#	var collider = right_ray.get_collider()
