27ce2153cafea7fd50754f87ea770de6d1d25e2e
Bach Le <bachle098@gmail.com>
2025-07-27 19:24:34 -0700

    pixelization, simple dungeon

--- label.gd
+++ label.gd
@@ -2,6 +2,6 @@ extends Label
 
 func _process(_delta: float) -> void:
 	$".".text = "LOC:"
-	$".".text += str(round($"../../char".global_transform.origin.x))
+	$".".text += str(round($"../../SubViewportContainer/SubViewport/char".global_transform.origin.x))
 	$".".text += " "
-	$".".text += str(round($"../../char".global_transform.origin.z))
+	$".".text += str(round($"../../SubViewportContainer/SubViewport/char".global_transform.origin.z))
